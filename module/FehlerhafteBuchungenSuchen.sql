0aff4ad81e8bec1840867d2849a15895543db041
Wolfgang <mail@w-werner.de>
2022-07-30 08:59:26 +0200

    "Fehlerhafte Buchungen suchen" erweitert

--- module/FehlerhafteBuchungenSuchen.sql
+++ module/FehlerhafteBuchungenSuchen.sql
@@ -1,4 +1,5 @@
 select LaufendeNr,
+       journal.Belegnummer,
        journal.Konto_nach,
        journal.BankNr,
        Journal.PersonenID,
@@ -8,6 +9,7 @@ left join personen on personen.PersonenID = Journal.PersonenID
 left join konten   on konten.KontoNr      = Journal.Konto_nach
 where (Buchungsjahr = :BJahr) and
       ((journal.Konto_nach not in (select konten.KontoNr from konten)) or
+	   (journal.Konto_nach = journal.BankNr) or
        (journal.BankNr not in (select konten.KontoNr from konten)) or
        ((Journal.PersonenID not in (select personen.PersonenID from Personen)) and (Journal.PersonenID > 0)) or
        ((journal.Betrag > 0) and (konten.Kontotype = 'A')) or
