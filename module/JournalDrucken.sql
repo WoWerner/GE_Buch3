a34622c65858282aae503f5958be0b2c5b49d192
Wolfgang <mail@w-werner.de>
2022-10-19 19:48:51 +0200

    Journalexport verbessert

--- module/JournalDrucken.sql
+++ module/JournalDrucken.sql
@@ -1,19 +1,20 @@
 select
    LaufendeNr,
-   strftime('%d.%m.%Y',Datum)                                     as Datum,
-   ifnull(Betrag,'')                                              as Betrag,
-   ifnull(Personen.Vorname,'')||' '||ifnull(Personen.Nachname,'') as Name,
-   Buchungstext,
-   Bemerkung,
    Belegnummer,
-   (select '('||ifnull(journal.konto_nach,'')||') '||ifnull(konten.Name,'') from konten where journal.konto_nach = konten.KontoNr) as Sachkonto,
-   (select name from konten where journal.BankNr = konten.KontoNr) as Bank,
-   CAST((select Anfangssaldo from Bankenabschluss where journal.BankNr = Bankenabschluss.KontoNr) as text) as Saldo, 
+   strftime('%d.%m.%Y',Datum)                                      as Datum,
    journal.BankNr as BankNr,
-   journal.konto_nach as konto_nach
+   (select name from konten where journal.BankNr = konten.KontoNr) as Bank,
+   journal.konto_nach                                              as konto_nach,
+   (select '('||ifnull(journal.konto_nach,'')||') '||ifnull(konten.Name,'') from konten where journal.konto_nach = konten.KontoNr) as Sachkonto,
+   ifnull(Betrag,'')                                               as Betrag,
+   Buchungstext,
+   ifnull(Personen.Vorname,'')||' '||ifnull(Personen.Nachname,'')  as Name,
+   Bemerkung,
+   CAST((select Anfangssaldo from Bankenabschluss where journal.BankNr = Bankenabschluss.KontoNr) as text) as Saldo, 
+   (select Steuer from konten where journal.konto_nach = konten.KontoNr) as Steuer
 from journal
 
-left join Personen        on  journal.PersonenID   = Personen.PersonenID
+left join Personen on journal.PersonenID   = Personen.PersonenID
 
 where (journal.Buchungsjahr=:BJAHR) :AddWhere
 
