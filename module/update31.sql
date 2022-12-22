fecfda7ccb3b6d2fd699495b0fa5fc0bdf54e4a9
Wolfgang <mail@w-werner.de>
2022-12-22 18:08:04 +0100

    Update auf die 3.1 verbessert.

--- module/update31.sql
+++ module/update31.sql
@@ -1,5 +1,6 @@
 BEGIN TRANSACTION;
-alter table konten add column [Steuer] VARCHAR (30) NULL;
+alter table konten add column [Steuer] VARCHAR (30);
+update konten set steuer = "" where steuer isnull
 Update konten set Steuer = "Nicht umsatzsteuerbar" where kontotype="E";
 Update init set version = "3.1";
 COMMIT;
