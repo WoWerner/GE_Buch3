e898883ac93390e8ca820908176f629f28815f12
Wolfgang <mail@w-werner.de>
2023-01-09 19:32:38 +0100

    Datenbank V3.2

--- module/update32.sql
+++ module/update32.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
-INSERT OR REPLACE INTO konten (KontoNr, Sortpos, Statistik, Name, Kontotype, Kontostand)  VALUES (999, 999, 999, "Ausgleichskonto für Kredite", "B", 0);
+INSERT OR REPLACE INTO konten (KontoNr, Sortpos, Statistik, Name, Kontotype, Kontostand)  VALUES (999, 999, 99, "Ausgleichskonto für Kredite", "B", 0);
 INSERT OR REPLACE INTO BankenAbschluss (KontoNr, Buchungsjahr, Anfangssaldo, Abschlusssaldo) VALUES (999, 9999, 0, 0);
+INSERT OR REPLACE INTO konten (KontoNr, Sortpos, Statistik, Name, Kontotype, Kontostand)  VALUES (998, 998, 99, "Ausgleichskonto für Depots", "A", 0);
 Update init set version = "3.2";
 COMMIT;
 vacuum;
