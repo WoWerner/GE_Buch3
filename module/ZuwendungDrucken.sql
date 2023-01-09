01ed8f8b2132f52a52796fcf5a09f4a4d839c989
Wolfgang <mail@w-werner.de>
2023-01-09 09:46:37 +0100

    Zuswendungsbescheinigungen einzeln

--- module/ZuwendungDrucken.sql
+++ module/ZuwendungDrucken.sql
@@ -4,6 +4,7 @@ left join journal on journal.PersonenID = Personen.PersonenID
 left join konten on konten.KontoNr = journal.konto_nach
 where ((journal.BuchungsJahr=:BJAHR) and 
       (((journal.konto_nach like '2%') and (konten.kontotype = "D") and (journal.Betrag>0)) or 
-	    (journal.konto_nach like '8%')))
+	    (journal.konto_nach like '8%'))
+		:ADDWHERE) 
 group by Personen.PersonenID
 order by nachname COLLATE NOCASE, vorname COLLATE NOCASE, Ort COLLATE NOCASE, strasse COLLATE NOCASE
