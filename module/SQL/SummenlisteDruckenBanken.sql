6e2899231826573bb856fa4221a1bdca602472a6
Wolfgang Werner <mail@w-werner.de>
2024-02-11 18:04:25 +0100

    Summenliste: Ausgleichskonten entfernt

--- module/SQL/SummenlisteDruckenBanken.sql
+++ module/SQL/SummenlisteDruckenBanken.sql
@@ -5,5 +5,7 @@ select  konten.Name,
         bankenabschluss.Abschlusssaldo 
 from konten
 left join bankenabschluss on konten.KontoNr=bankenabschluss.KontoNr
-where (bankenabschluss.BuchungsJahr=:BJAHR) and (konten.kontotype = "B")
-order by konten.KontoNr
+where (bankenabschluss.BuchungsJahr=:BJAHR) and 
+      (konten.kontotype = "B") and 
+			(konten.statistik < 99)
+order by konten.Sortpos
